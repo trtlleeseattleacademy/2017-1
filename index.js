30e2311bbf5f834ebcbc1b2dc6df66dfdb826575
trtllee <trtllee@seattleacademy.org>
2020-02-05 09:14:43 -0800

    Add Counter to Server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+const express = require('express')
+const app = express()
+const port = 1514
+let counter = 0
+
+app.get('/', (req, res) => res.send('Hello Trtl!'))
+
+app.get('/counter', function (req, res) {
+  res.send('counter' + counter);
+})
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
